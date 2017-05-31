e6a848435eced5682780a518e08a47e7e7d09a20
pvozila <pvozila@tvz.hr>
2017-05-31 15:34:53 +0200

    v 1.0.0.
    inicijalno dodavanje file-ova, kreiran login, logout, index (main menu) koji vodi dalje na sekcije aplikacije, te je dodan .css file i xml datoteka za admin usera

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<link rel="icon" href="favicon.ico" type="image/x-icon"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+	<title> Devpool App </title>
+</head>
+<body>
+	<div class="wrapper">
+		<img class="logo" src="5863ef2b7d90850fc3ce296a.png" width="100px" height="100px"/>
+		<div class="logout_link"><p><a href="logout.php"> Odjava </a></p></div>
+		<div class="mainmenu">
+			<div class="mainmenu1"><a href="#"> Kreiraj novu anketu </a></div>
+			<div class="mainmenu2"><a href="#"> Administracija ankete </a></div>
+			<div class="mainmenu3"><a href="#"> Pregled odgovora </a></div>
+			<div class="mainmenu4"><a href="#"> Podijeli anketu </a></div>
+		</div>
+		
+	</div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,57 @@
+<?php
+
+$error = false;
+if(isset($_POST['submit'])){
+	$kor_ime = preg_replace('/[^A-Za-z]/', '', $_POST['kor_ime']);
+	$lozinka = md5($_POST['lozinka']);
+	if(file_exists('users/' . $kor_ime . '.xml')){
+		$xml = new SimpleXMLElement('users/' . $kor_ime . '.xml', 0, true);
+		if($lozinka == $xml->password){
+			session_start();		
+			$_SESSION['kor_ime'] = $kor_ime;
+			header('Location: index.php');
+			die;
+		}
+	}
+	$error = true;
+}	
+
+?>
+
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<link rel="icon" href="favicon.ico" type="image/x-icon"/>
+	<title> Devpool App </title>
+</head>
+<body>
+	<div class="wrapper">
+		<img class="logo" src="5863ef2b7d90850fc3ce296a.png" width="100px" height="100px"/>
+		<form action="" method="post">
+			<table>
+				<tr>	
+					<td>
+						<?php
+							if($error){
+								echo '<p style="color:red;">Pogrešno korisničko ime i/ili lozinka!</p>';
+							}
+						?>	
+					</td>
+				</tr>
+				<tr>
+					<td align="center"><input class="login_input_css" type="text" name="kor_ime" id="kor_ime" placeholder="Unesi korisnicko ime..." align="center"/></td>
+				</tr>
+				<tr>
+					<td align="center"><input class="login_input_css" type="password" name="lozinka" id="lozinka" placeholder="Unesi lozinku..."/></td>
+				</tr>
+				<tr>
+					<td align="center"><input class="login_input_buttons_css" type="submit" name="submit" id="submit" value="Potvrdi"/></td>
+				</tr>
+				<tr>
+					<td align="center"><input class="login_input_buttons_css" type="reset" name="reset" id="reset" value="Očisti polja"/></td>
+				</tr>
+			</table>
+		</form>
+	</div>
+</body>
+</html>
